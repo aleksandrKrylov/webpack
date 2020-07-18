7ed7928f88cc54a036fbb0194f8aa36a0d9dae37
Александр <a.v.krylovv@yandex.ru>
2020-07-18 17:55:06 +0300

    2 commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-(function() {
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
+
+console.log(doubledNumbers); // 4, 6, 10
+import "./pages/index.css";
+
+/*(function() {
   const infoTokenId =  { token: '47bb72d3-cbf1-498a-9fe2-5d7f7327faa0', urlId: 'https://praktikum.tk/cohort11' };
   const userInfoPhoto = document.querySelector('.user-info__photo')
   const placesList = document.querySelector('.places-list');
@@ -65,54 +72,4 @@
       console.log( `Ошибка: ${err}`);
     });  
   });
-})();
-
-/*
-  Неплохая работа, данные с сервера приходят и профиль пользователя сохраняется
-  Но есть несколько замечаний
-
-  Надо исправить:
-  - + все изменения на странице должны происходить, только после того, как
-  сервер ответил подтверждением  - сохранение профиля на странице и закрытие попапа 
-  делать только после ответа сервера
-
-  Можно лучше:
-  - + базовый адрес сервера так же лучше передавать как параметр 
-  конструктора, а не хардкодить в методах
-
-  - + проверка ответа сервера и преобразование из json
-  дублируется во всех методах класса Api, лучше вынести всю её в отдельный метод
-
-  - для загрузки начальный данных лучше применить Promise.all
-    https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-
-    Выглядит этот код примерно так:
-    Promise.all([     //в Promise.all передаем массив промисов которые нужно выполнить
-      this.api.getInfoAboutUser(),
-      this.api.getInitialCards()
-    ])    
-      .then((values)=>{    //попадаем сюда когда оба промиса будут выполнены
-        const [userData, initialCards] = values;
-        ....................
-      })
-      .catch((err)=>{     //попадаем сюда если один из промисов завершаться ошибкой
-        ....................
-      })
-
-  - + переименовать skript.js в script.js
-*/
-
-/*
-  Отлично, критические замечания исправлены
-
-  Для закрепления полученных знаний советую сделать и оставшуюся часть задания.
-  
-  Если у Вас будет свободное время попробуйте освоить работу с сервером
-  применив async/await для работы с асинхронными запросами.
-  https://learn.javascript.ru/async-await
-  https://habr.com/ru/company/ruvds/blog/414373/
-  https://www.youtube.com/watch?v=SHiUyM_fFME
-  Это часто используется в реальной работе
-
-  Успехов в дальнейшем обучении!
-*/
+})(); */
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+      rules: [{ 
+          test: /\.js$/, 
+          exclude: /node_modules/,
+           /* use: { loader: "babel-loader" },
+           exclude: /node_modules/ */
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                {
+                  plugins: [
+                    '@babel/plugin-proposal-class-properties'
+                  ]
+                }
+              ]
+            }
+          }
+              },
+              {
+              test: /\.css$/, 
+              use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] 
+            },
+            {
+              test: /\.(woff|woff2|ttf)$/,
+              use: 'file-loader?name=./fonts/[name].[ext]'
+            },
+            {
+              test: /\.(jpg|jpeg|png|svg|webp)$/,
+              use: 'file-loader?name=./images/[name].[ext]&esModule=false'
+            }
+          ]
+      },
+      plugins: [
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+        new HtmlWebpackPlugin({
+          inject: false, 
+          template: './src/index.html', 
+          filename: 'index.html' 
+        }),
+        new WebpackMd5Hash()
+    ]
+}
